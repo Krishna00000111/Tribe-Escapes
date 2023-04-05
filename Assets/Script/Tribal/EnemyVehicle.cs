446009ca98685fa6aad780cd3be8fb4326364830
Krishna00000111 <86921469+Krishna00000111@users.noreply.github.com>
2023-04-05 14:40:59 +0530

    boat chase

--- Assets/Script/Tribal/EnemyVehicle.cs
+++ Assets/Script/Tribal/EnemyVehicle.cs
@@ -6,15 +6,37 @@ public class EnemyVehicle : MonoBehaviour
 {
     private GameObject target; 
     public float speed = 5f;
+    private float stoppingDistance = 5f;
+
+    private Rigidbody vehicleRb;
 
     private void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player");
+        vehicleRb = GetComponent<Rigidbody>();
+        speed = Random.Range(6f, 7f);
+
     }
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
-        transform.LookAt(target.transform.position); 
+        //transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+
+
+        if (target != null)
+        {
+            Vector3 direction = target.transform.position - transform.position;
+            float distance = direction.magnitude;
+            if (distance > stoppingDistance)
+            {
+                Vector3 velocity = direction.normalized * speed;
+                vehicleRb.velocity = velocity;
+            }
+            else
+            {
+                vehicleRb.velocity = Vector3.zero;
+            }
+        }
+        transform.LookAt(target.transform.position);
     }
 }
