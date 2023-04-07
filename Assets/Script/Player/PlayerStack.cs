eee36f4bb0645d97c124d18a0b700c4cdcda2579
Krishna00000111 <86921469+Krishna00000111@users.noreply.github.com>
2023-04-07 14:30:51 +0530

    stack underprocess

--- Assets/Script/Player/PlayerStack.cs
+++ Assets/Script/Player/PlayerStack.cs
@@ -1,24 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStack : MonoBehaviour
 {
+    public Transform holderParent;
 
-    public GameObject stackablePrefab;
+    Stack<Transform> collectedTrans = new Stack<Transform>();
+    bool isIndropArea;
+    Vector3 DropPos;
+    //unlock Reward/Item
 
-    private float stackHeight = 0;
-
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Stackable")
+        if (other.gameObject.CompareTag("Drop"))
         {
-            //Create a new instance of the stackable prefab
-            GameObject newStackObject = Instantiate(stackablePrefab);
-            //Position the object on top of the currently stacked objects
-            newStackObject.transform.position = new Vector3(transform.position.x, transform.position.y + stackHeight, transform.position.z);
-            //Update the stack height
-            stackHeight += newStackObject.GetComponent<Renderer>().bounds.size.y;
-            //Make the new object a child of the player object
-            newStackObject.transform.parent = transform;
+            isIndropArea = true;
+            DropPos = other.transform.position;
+            //other.TryGetComponent(out unlockItemSrc);
+
+            StartCoroutine(DropSlow(0.5f));
         }
+
+        else
+        {
+            Item localItem = null;
+            if (other.gameObject.CompareTag("Pickable") && localItem && !localItem.isCollected)
+            {
+                other.transform.SetParent(holderParent);
+                other.transform.localPosition = new Vector3(0, collectedTrans.Count * .25f, 0.1f);
+                other.transform.localRotation = holderParent.rotation;
+
+                collectedTrans.Push(other.transform);
+                localItem.isCollected = true;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Drop"))
+        {
+            isIndropArea = false;
+        }
+    }
+
+    IEnumerator DropSlow(float _delay = 0)
+    {
+        while (isIndropArea)
+        {
+            yield return new WaitForSeconds(_delay);
+            if (collectedTrans.Count > 0)
+            {
+                Transform newItem = collectedTrans.Pop();
+                newItem.parent = null;
+            }
+        }
+
+        yield return null;
     }
 }
